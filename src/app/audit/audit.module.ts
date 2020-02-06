24a504be142c2e8fd93689ab5fa881460d091dc7
eamonboyle <blaowskate@hotmail.com>
2020-02-06 15:47:43 +0000

    Add corodova camera plugin

--- src/app/audit/audit.module.ts
+++ src/app/audit/audit.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
+import { Camera } from '@ionic-native/camera/ngx';
+
 import { SharedModule } from '../shared/shared.module';
 
 import { AuditPageRoutingModule } from './audit-routing.module';
@@ -11,13 +13,45 @@ import { AuditPageRoutingModule } from './audit-routing.module';
 import { AuditPage } from './audit.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    AuditPageRoutingModule,
-	SharedModule
-  ],
-  declarations: [AuditPage]
+	providers: [Camera],
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		AuditPageRoutingModule,
+		SharedModule
+	],
+	declarations: [AuditPage]
 })
-export class AuditPageModule {}
+export class AuditPageModule {
+	base64Images: string[];
+	
+	constructor(private camera: Camera) {
+		
+	}
+	
+	submit() {
+        this.itemRef.update({
+            ...this.model,
+            photos: this.base64Images,
+            status: this.form.invalid ? 'incomplete' : 'complete',
+        });
+        this.router.navigateByUrl('/home');
+    }
+
+	
+	openCamera() {
+        const options: CameraOptions = {
+            quality: 100,
+            cameraDirection: this.camera.Direction.BACK,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        };
+
+        this.camera.getPicture(options).then((imageData) => {
+            this.base64Images.push(`data:image/jpeg;base64,${imageData}`);
+        });
+    }
+	
+}
